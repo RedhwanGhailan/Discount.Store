61c0813f730dcac230c1f75d379af4a04ca5491a
Redhwan Ghailan <Redhwan.Ghailan@diamir.se>
2022-02-17 08:49:48 +0100

    Add DI
    Add DI, Ioc of the project using StructureMap

---
+++ Discount.Store/Discount.Store/Ioc.cs
@@ -0,0 +1,22 @@
+﻿using Discount.Store.Services;
+using StructureMap;
+
+namespace Discount.Store
+{
+    internal static class Ioc
+    {
+        internal static void Configure(IContainer container)
+        {
+            container.Configure(config =>
+            {
+                config
+                    .For<IMainService>()
+                    .Use<MainService>();
+
+                config
+                   .For<ICartService>()
+                   .Use<CartService>();
+            });
+        }
+    }
+}
