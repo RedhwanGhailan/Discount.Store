6b1a42a6eb966bcb9fc34cbc6bc8ce209276aa4b
Redhwan Ghailan <Redhwan.Ghailan@diamir.se>
2022-02-17 08:50:30 +0100

    Program Class
    Update the program class

--- Discount.Store/Discount.Store/Program.cs
+++ Discount.Store/Discount.Store/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discount.Store.Services;
+using StructureMap;
+using System;
 
 namespace Discount.Store
 {
-    class Program
+    internal class Program
     {
+        /// <summary>
+        /// Discount Store 
+        /// 
+        /// </summary>
+
+        private static IContainer Container { get; set; } = new Container();
+
         static void Main(string[] args)
         {
+            Ioc.Configure(Container);
+            MainService mainService;
+
+            try
+            {
+                mainService = Container.GetInstance<MainService>();
+                mainService.StartProcess();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Fatal error when setting up instance: {e}");
+            }
+
+            Console.WriteLine("Done. Enter to exit.");
+            Console.ReadLine();
         }
     }
 }
