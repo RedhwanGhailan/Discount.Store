71b2e4c53b2f48be817eb0cac1117732bedfc874
Redhwan Ghailan <Redhwan.Ghailan@diamir.se>
2022-02-17 08:43:47 +0100

    Add Dto
    Add the Dto if the Item

---
+++ Discount.Store/Discount.Store/Dto/Item.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Discount.Store.Dto
+{
+    public class Item
+    {
+        [JsonProperty("itemNumber")]
+        public int Number;
+
+        [JsonProperty("itemName")]
+        public string Name;
+
+        [JsonProperty("itemPrice")]
+        public decimal Price;
+
+        [JsonProperty("itemCurrency")]
+        public string Currency;
+
+        [JsonProperty("itemDiscount")]
+        public ItemDiscount Discount;
+
+        [JsonProperty("itemOfferDiscount")]
+        public bool OfferDiscount;
+    }
+}
---
+++ Discount.Store/Discount.Store/Dto/ItemDiscount.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Discount.Store.Dto
+{
+    public class ItemDiscount
+    {
+        [JsonProperty("quantity")]
+        public int Quantity;
+
+        [JsonProperty("newPrice")]
+        public decimal NewPrice;
+    }
+}
